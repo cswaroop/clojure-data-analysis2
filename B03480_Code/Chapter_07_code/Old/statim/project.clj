e56c0a4b1370baf32229531fa969d2b0f19cbdfd
cswaroop <cswaroop@gmail.com>
2015-03-11 15:25:08 +0530

    third

---
+++ B03480_Code/Chapter_07_code/Old/statim/project.clj
@@ -0,0 +1,7 @@
+(defproject statim "0.1.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter "1.5.5"]])
+                 ; [incanter/incanter-zoo "1.5.5"]])
+                 ; [org.clojure/data.csv "0.1.2"]])
+                 ; [org.apache.directory.studio/org.apache.commons.collections "3.2.1"]
+                 ; [clj-time "0.9.0-beta1"]
